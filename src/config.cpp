388b954c9794d595c549d12c0efc35844e5aeb66
wowjinxy <1340990+wowjinxy@users.noreply.github.com>
2024-10-03 11:01:56 -0500

    Create config.cpp

---
+++ src/config.cpp
@@ -0,0 +1,106 @@
+namespace openre::config
+{
+    // 0X00449FD0
+    void Config(void) {}
+    
+    // 0x0044a040
+    // FUN_0044a040
+
+    // 0x0044A0B0
+    void Config_main(void) {}
+
+    // 0x0044ad40
+    // FUN_0044ad40
+
+    // 0x0044ad40
+    void Config_set_data(void) {}
+
+    // 0x0044aed0
+    // FUN_0044aed0
+
+    // 0x0044af00
+    // FUN_0044af00
+
+    // 0x0044b020
+    // FUN_0044b020
+
+    // 0x0044b100
+    // FUN_0044b100
+
+    // 0x0044b180
+    // FUN_0044b180
+
+    // 0x0044b3c0
+    // FUN_0044b3c0
+
+    // 0x0044ba90
+    // FUN_0044ba90
+
+    // 0x0044bb10
+    // FUN_0044bb10
+
+    // 0x0044bb90
+    // FUN_0044bb90
+
+    // 0x0044bc10
+    void Config_draw_smode(void) {}
+
+    void Config_draw_monoraul(void) {}
+
+    void Config_draw_stereo(void) {}
+
+    void Config_draw_sexit(void) {}
+
+    void Config_draw_sound(void) {}
+
+    void Config_draw_bgmvol(void) {}
+
+    void Config_draw_sevol(void) {}
+
+    // 0x0044c570
+    // FUN_0044c570
+
+    void DrawConfigTim(void) {}
+
+    void Config_draw_key(void) {}
+
+    void Config_draw_button(void) {}
+
+    // 0x0044d630
+    // FUN_0044d630
+
+    // 0x0044d890
+    // FUN_0044d890
+
+    // 0x0044d990
+    // FUN_0044d990
+
+    // 0x0044da40
+    // FUN_0044da40
+
+    // 0x0044dab0
+    // FUN_0044dab0
+
+    // 0x0044db30
+    // FUN_0044db30
+
+    void Config_draw_squality(void) {}
+
+    //??? These may not exist in the PC version
+    //void Config_fade_in_wait(void) {}
+    //void Config_fade_out_set(void) {}
+    //void Config_fade_out_wait(void) {}
+    //void Config_init(void) {}
+    //void Config_exit(void) {}
+    //void Config_padd_main(void) {}
+    //void Config_padd_opt(void) {}
+    //void Config_padd_con(void) {}
+    //void Config_padd_snd(void) {}
+    //void Config_padd_clr(void) {}
+    //void Config_padd_res(void) {}
+    //void Config_move(void) {}
+    //void Config_main_trans(void) {}
+    //void Config_suuji_trans(void) {}
+    //void Config_lrgb(void) {}
+    //void Config_suuji(void) {}
+}
