02765edb8e08f24655ad3a8573c98471fb9744f5
Elias Martinez <mselias97@gmail.com>
2025-03-31 23:23:18 +0200

    Implement get_input_device_state

--- src/input.cpp
+++ src/input.cpp
@@ -62,6 +62,12 @@ namespace openre::input
         return gamepadState;
     }
 
+    enum
+    {
+        INPUT_DEVICE_KEYBOARD,
+        INPUT_DEVICE_CONTROLLER
+    };
+
     // 0x00410450
     void on_key_up(int keyCode, int a1)
     {
@@ -92,23 +98,35 @@ namespace openre::input
         return gGameTable.input.keyboard;
     }
 
-    static uint32_t dword_524CE8[32] = {
+    static uint32_t input_keyboard_data[32] = {
         0x1000, 0x4000, 0x8000, 0x2000, 0x20, 0x44, 0x2, 0x10, 0x4, 0x1,    0x8,    0x80,   0x100,  0x800, 0,    0,
         0,      0,      0,      0,      0,    0,    0,   0,    0,   0x1000, 0x4000, 0x8000, 0x2000, 0x80,  0x80, 0x40,
     };
 
+    static uint32_t input_controller_data[32] = {
+        0x1000, 0x4000, 0x8000, 0x2000, 0, 0, 0, 0, 0x80, 0x44, 0x8, 0x800, 0x10, 0x100, 0x2, 0,
+        0x2,    0,      0,      0,      0, 0, 0, 0, 0,    0,    0,   0,     0,    0,     0,   0,
+    };
+
     // 0x0043BAC0
-    int sub_43BAC0(int a0, int a1)
+    int get_input_device_state(int a0, int inputType)
     {
-        auto result = 0;
+        auto inputState = 0;
         for (int i = 0; i < 32; i++)
         {
             if (a0 & (1 << i))
             {
-                result |= dword_524CE8[a1 + i];
+                if (inputType == INPUT_DEVICE_KEYBOARD)
+                {
+                    inputState |= input_keyboard_data[i];
+                }
+                else if (inputType == INPUT_DEVICE_CONTROLLER)
+                {
+                    inputState |= input_controller_data[i];
+                }
             }
         }
-        return result;
+        return inputState;
     }
 
     // 0x004100F0
@@ -127,7 +145,7 @@ namespace openre::input
         joy_get_pos_ex(gGameTable.input.mapping);
         if (gGameTable.input.var_1F8 != 0)
         {
-            v1 = sub_43BAC0(gGameTable.input.var_24, 0);
+            v1 = get_input_device_state(gGameTable.input.var_24, INPUT_DEVICE_KEYBOARD);
 
             gGameTable.dword_99CF64 = gGameTable.input.var_24;
             gGameTable.dword_66D394 = v1;
@@ -135,7 +153,7 @@ namespace openre::input
         gGameTable.dword_99CF70 = 0;
         if (gGameTable.input.var_3B24 >= 2 && gGameTable.input.var_3D0 != 0)
         {
-            auto v2 = sub_43BAC0(gGameTable.input.var_1FC, 1);
+            auto v2 = get_input_device_state(gGameTable.input.var_1FC, INPUT_DEVICE_CONTROLLER);
             v1 |= v2;
             gGameTable.dword_99CF70 = v2;
             gGameTable.dword_66D394 = v1;
@@ -146,9 +164,9 @@ namespace openre::input
 
     void input_init_hooks()
     {
-        writeJmp(0x00410450, on_key_up);
-        writeJmp(0x00410410, on_read_key);
-        writeJmp(0x00410400, input_get_some_byte);
-        writeJmp(0x0043BB00, sub_43BB00);
+        writeJmp(0x00410450, &on_key_up);
+        writeJmp(0x00410410, &on_read_key);
+        writeJmp(0x00410400, &input_get_some_byte);
+        writeJmp(0x0043BB00, &sub_43BB00);
     }
 };
