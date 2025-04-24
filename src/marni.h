5292ba76c86312eaa4c681180f3af229561ff766
Elias Martinez <mselias97@gmail.com>
2025-04-24 16:52:22 +0200

    Fix wrong unload_texture_page signature

--- src/marni.h
+++ src/marni.h
@@ -8,7 +8,7 @@ namespace openre::marni
     void out();
     void unload_door_texture();
     bool sub_442E40();
-    int unload_texture_page(int page);
+    void unload_texture_page(int page);
     void door_disp0(int doorId, int a1, int a2, int a3);
     void door_disp1(int doorId);
     void result_unload_textures();
