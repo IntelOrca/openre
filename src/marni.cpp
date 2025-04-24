9366fefc1239acac8efed44297f66e68f8a7ef7c
Elias Martinez <mselias97@gmail.com>
2025-04-24 16:16:57 +0200

    Code formatting

--- src/marni.cpp
+++ src/marni.cpp
@@ -56,7 +56,7 @@ namespace openre::marni
             = { 0, 1, 2, 3, 4, 5, 6, 7, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33 };
         for (const auto page : pages)
         {
-            unload_texture_page(pages[page]);            
+            unload_texture_page(pages[page]);
         }
     }
 
