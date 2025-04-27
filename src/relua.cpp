7469a506bfccaa62533388183fe9183f9d303447
Ted John <ted@brambles.org>
2025-04-27 17:12:39 +0100

    Use enum table for hook kind

--- src/relua.cpp
+++ src/relua.cpp
@@ -6,6 +6,8 @@
 #include <lua.hpp>
 #include <memory>
 #include <string>
+#include <string_view>
+#include <tuple>
 
 namespace fs = std::filesystem;
 
@@ -70,29 +72,97 @@ namespace openre::lua
         void setupGlobals()
         {
             auto L = _state;
-
-            lua_newtable(L);
-            lua_setglobal(L, "re");
-
-            defineApi("subscribe", apiSubscribe);
-            defineApi("getFlag", apiGetFlag);
-            defineApi("setFlag", apiSetFlag);
-            setGlobal("HOOK_TICK", 1);
+            setGlobal("re.subscribe", apiSubscribe);
+            setGlobal("re.getFlag", apiGetFlag);
+            setGlobal("re.setFlag", apiSetFlag);
+            setGlobal("HookKind.tick", 1);
         }
 
-        void setGlobal(const char* name, int32_t value)
+        void setGlobal(std::string_view fullName, int32_t value)
         {
+            auto [ns, name] = SplitNamespace(fullName);
+            getOrCreateAndPushGlobal(ns);
+            lua_pushlstring(_state, name.data(), name.size());
             lua_pushinteger(_state, value);
-            lua_setglobal(_state, name);
+            lua_settable(_state, -3);
+            lua_pop(_state, 1);
         }
 
-        void defineApi(const char* name, lua_CFunction f)
+        void setGlobal(std::string_view fullName, lua_CFunction f)
         {
-            lua_getglobal(_state, "re");
-            lua_pushstring(_state, name);
+            auto [ns, name] = SplitNamespace(fullName);
+            getOrCreateAndPushGlobal(ns);
+            lua_pushlstring(_state, name.data(), name.size());
             lua_pushlightuserdata(_state, this);
             lua_pushcclosure(_state, f, 1);
             lua_settable(_state, -3);
+            lua_pop(_state, 1);
+        }
+
+        void getOrCreateAndPushGlobal(std::string_view s)
+        {
+            lua_pushglobaltable(_state);
+            while (!s.empty())
+            {
+                auto part = s;
+                auto delim = s.find('.');
+                if (delim == std::string::npos)
+                {
+                    s = "";
+                }
+                else
+                {
+                    part = s.substr(0, delim);
+                    s = s.substr(delim + 1);
+                }
+
+                lua_pushlstring(_state, part.data(), part.size());
+                lua_gettable(_state, -2);
+                if (!lua_istable(_state, -1))
+                {
+                    lua_pop(_state, 1);
+                    lua_newtable(_state);
+                    lua_pushlstring(_state, part.data(), part.size());
+                    lua_pushvalue(_state, -2);
+                    lua_settable(_state, -4);
+                }
+                lua_remove(_state, -2);
+            }
+        }
+
+        void printStack()
+        {
+            auto top = lua_gettop(_state);
+            std::printf("Lua Stack (size: %d):\n", top);
+            for (auto i = 1; i <= top; i++)
+            {
+                auto type = lua_type(_state, i);
+                printf("%d: ", i);
+                switch (type)
+                {
+                case LUA_TSTRING: printf("string: %s", lua_tostring(_state, i)); break;
+                case LUA_TNUMBER: printf("number: %f", lua_tonumber(_state, i)); break;
+                case LUA_TBOOLEAN: // Boolean
+                    printf("boolean: %s", lua_toboolean(_state, i) ? "true" : "false");
+                    break;
+                case LUA_TTABLE: printf("table"); break;
+                case LUA_TFUNCTION: printf("function"); break;
+                case LUA_TUSERDATA: printf("userdata"); break;
+                case LUA_TTHREAD: printf("thread"); break;
+                case LUA_TNIL: printf("nil"); break;
+                default: printf("unknown"); break;
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+
+        static std::tuple<std::string_view, std::string_view> SplitNamespace(std::string_view s)
+        {
+            auto delim = s.find_last_of('.');
+            auto ns = delim == std::string::npos ? "" : s.substr(0, delim);
+            auto name = s.substr(delim + 1);
+            return { ns, name };
         }
 
         static int apiSubscribe(lua_State* L)
