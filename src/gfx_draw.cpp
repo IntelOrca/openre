7f8a27ae890894020c0c3c98ab39d864198fe5b2
Ted John <ted@brambles.org>
2026-08-08 18:28:33 +0100

    Decompile AddLineF2 (0x441370) into gfx_draw
    Flat 2-vertex line written as a PrimLine. Guards on x0 >= 0 and
    scratch space; uses type 0x200011 when code&2 && tag==1; packs
    colour and inserts via add_primitive_front/back.

--- src/gfx_draw.cpp
+++ src/gfx_draw.cpp
@@ -360,8 +360,32 @@ namespace openre::gfx_draw
     // 0x00441370
     int add_line_f2(LineF2* p, int z, int is_back)
     {
-        // TODO(decompiler): implement
-        return 0;
+        // The line is written into the shared scratch buffer as a flat
+        // 2-vertex line primitive (20 bytes, see PrimLine in re2.h).
+        PrimLine* prim = (PrimLine*)scratch_ptr();
+
+        if (p->x0 < 0)
+            return 0;
+        if ((uintptr_t)(prim + 1) > (uintptr_t)scratch_end())
+            return 0;
+
+        prim->type = 17;
+        if ((p->code & 2) != 0 && p->tag == 1)
+            prim->type = 0x200011;
+        prim->x0 = p->x0;
+        prim->y0 = p->y0;
+        prim->x1 = p->x1;
+        prim->y1 = p->y1;
+        prim->color0 = (uint32_t)((p->r0 << 16) | (p->g0 << 8) | p->b0);
+
+        if (is_back)
+            marni::add_primitive_back(gGameTable.pMarni, (Prim*)prim, z);
+        else
+            marni::add_primitive_front(gGameTable.pMarni, (Prim*)prim, z);
+
+        ++gGameTable.dword_67C9CC;
+        gGameTable.off_524E1C = (uint32_t)(uintptr_t)(prim + 1);
+        return 1;
     }
 
     void init_hooks()
@@ -372,5 +396,6 @@ namespace openre::gfx_draw
         interop::writeJmp(0x00440280, &add_sprt);
         interop::writeJmp(0x00441270, &add_tile);
         interop::writeJmp(0x00440480, &add_sprt_v);
+        interop::writeJmp(0x00441370, &add_line_f2);
     }
 }
